292e89f23cefbcc0ffeea34cef99d0b3e1b2f1f1
AiinuShil3a <Supphalak.sm@outlook.com>
2023-11-09 05:17:11 +0700

    savepoine

--- src/component/page/productPage.jsx
+++ src/component/page/productPage.jsx
@@ -1,18 +1,17 @@
-import {useEffect , useState}from 'react'
-import {Axios} from '../../service/auth.context'
-import Loading from '../../isLoading/loadingPage'
-import animationLoading from '../../assets/videoJSON/loadingPage.json'
+import { useEffect, useState } from "react";
+import { Axios } from "../../service/auth.context";
+import Loading from "../../isLoading/loadingPage";
+import animationLoading from "../../assets/videoJSON/loadingPage.json";
 
 const ListPage = () => {
-
-  const [list , setList]= useState([]);
+  const [list, setList] = useState([]);
   const [loading, setLoading] = useState(true);
-  
+
   useEffect(() => {
-    const fetchlist = async() => {
+    const fetchlist = async () => {
       try {
         const res = await Axios.get(`/Carcenters`);
-        setList(res.data)
+        setList(res.data);
         setLoading(false);
       } catch (error) {
         console.log(error);
@@ -20,22 +19,30 @@ const ListPage = () => {
       }
     };
     fetchlist();
-  } , []);
+  }, []);
 
   return (
-    
     <div>
       {loading ? (
-        <Loading animation = {animationLoading}  />
+        <Loading animation={animationLoading} />
       ) : list !== null && list !== undefined ? (
-        <div className='recomment'>
-          
+        <div>
+          <div className="recomment"></div>
+          <div className="cardproduct">
+            {list.map((item, index) => (
+              <div className="cardlist" key={index}>
+                <img src={item.imageFirst} alt="Product" />
+                <div className="brand">{item.brand}</div>
+                <div className="price">{item.price}</div>
+              </div>
+            ))}
+          </div>
         </div>
       ) : (
         <h1 className="white-text">No data</h1>
       )}
     </div>
-  )
-}
+  );
+};
 
-export default ListPage
+export default ListPage;
