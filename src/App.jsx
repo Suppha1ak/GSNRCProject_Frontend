020c53cd4f32370247525f4adb41c4efb36b8c42
Suttiguy <guy.guy0205245@gmail.com>
2023-11-12 23:51:26 +0700

    sssss

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ const App = () => {
           <Routes>
             <Route path='/' element={<Index.HomePage />} />
             <Route path='/product' element={<Index.ProductPage />} />
-            <Route path='/create' element={<Index.CreatePage />} />
+            <Route path='/create' element={<ProtectedRoute> <Index.CreatePage /> </ProtectedRoute> } />
             <Route path='/update/:id' element={<Index.UpdatePage />} />
             <Route path='/detail/:id' element={<Index.DetailPage />} />
             <Route path='/about' element={<Index.AboutPage />} />
